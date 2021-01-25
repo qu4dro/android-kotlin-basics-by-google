947903bc8a97a2a3ce57067ced306815fe00f13b
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-01-25 14:20:06 +0800

    Scrollable list task solved

--- ScrollableList/app/src/main/java/ru/orlovvv/scrollablelist/MainActivity.kt
+++ ScrollableList/app/src/main/java/ru/orlovvv/scrollablelist/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.orlovvv.scrollablelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.orlovvv.scrollablelist.adapter.ItemAdapter
+import ru.orlovvv.scrollablelist.data.Datasource
+import ru.orlovvv.scrollablelist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myDataSet = Datasource().loadAffirmations()
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ ScrollableList/app/src/main/java/ru/orlovvv/scrollablelist/model/Affirmation.kt
@@ -0,0 +1,8 @@
+package ru.orlovvv.scrollablelist.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(@DrawableRes val imageResourceId: Int, @StringRes val stringResourceId: Int) {
+
+}
