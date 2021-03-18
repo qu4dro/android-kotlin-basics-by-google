3c8597e826b336dd269e857b2b6f3dbb746e3b76
unknown <qu4dro.sl@gmail.com>
2021-03-18 09:51:23 +0800

    Summary fragment cancel button logic added

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -22,6 +22,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentSummaryBinding
 
 /**
@@ -62,6 +63,11 @@ class SummaryFragment : Fragment() {
         Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
     }
 
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
+    }
     /**
      * This fragment lifecycle method is called when the view hierarchy associated with the fragment
      * is being removed. As a result, clear out the binding object.
