48bcf6ff8a4b100ec1936e8a63a8a258b2cb38c4
unknown <qu4dro.sl@gmail.com>
2021-03-23 08:07:07 +0800

    Updated with coroutines

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,11 +20,14 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.marsrealestate.network.MarsApi
 import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -49,16 +52,13 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(
-                object : Callback<List<MarsProperty>> {
-                    override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                        _response.value = response.body()?.size.toString()
-                    }
-
-                    override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                        _response.value = "Error: " + t.message
-                    }
-
-                })
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getProperties()
+                _response.value = listResult.size.toString()
+            } catch (e: Exception) {
+                _response.value = "Error: ${e.message}"
+            }
+        }
     }
 }
