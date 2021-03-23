87bae79a921622907cc48e76915fa423e55c00bd
unknown <qu4dro.sl@gmail.com>
2021-03-23 14:22:03 +0800

    Object converts added

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -16,6 +16,33 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.accounts.AuthenticatorDescription
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.DevByteVideo
+
+@Entity
+data class DatabaseVideo(
+        @PrimaryKey
+        val url: String,
+        val updated: String,
+        val title: String,
+        val description: String,
+        val thumbnail: String
+) {
+        fun List<DatabaseVideo>.asDomainModel() : List<DevByteVideo> {
+                return map {
+                        DevByteVideo(
+                                url = it.url,
+                                title = it.title,
+                                description = it.description,
+                                updated = it.updated,
+                                thumbnail = it.thumbnail
+                        )
+                }
+        }
+}
+
 
 /**
  * Database entities go in this file. These are responsible for reading and writing from the
