de09de476a20b80d6917a01d9b75460896f0932e
unknown <qu4dro.sl@gmail.com>
2021-03-24 12:48:07 +0800

    Caching and refresh added

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -36,17 +36,18 @@ abstract class VideosDatabase: RoomDatabase() {
 
     abstract val videoDao: VideoDao
 
-    private lateinit var INSTANCE: VideosDatabase
+}
 
-    fun getDatabase(context: Context): VideosDatabase {
-        synchronized(VideosDatabase::class.java) {
-            if (!::INSTANCE.isInitialized) {
-                INSTANCE = Room.databaseBuilder(context.applicationContext,
-                        VideosDatabase::class.java,
-                        "videos").build()
-            }
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    VideosDatabase::class.java,
+                    "videos").build()
         }
-        return INSTANCE
     }
+    return INSTANCE
 }
 
